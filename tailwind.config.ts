1d7b8c74e0074cfc1e142af4c82b088849e6a94a
PenPow <josh@penpow.dev>
2024-01-07 12:32:36 +0000

    chore: move to ts configs
    astro and tailwind now use typescript for config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
+import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -12,4 +12,4 @@ export default {
 	},
 	darkMode: 'class',
 	plugins: [],
-}
+} satisfies Config
